da9114cf03083aed87ab69970715ceeb63bd3cb7
Andreas Waldis <andreas.waldis@live.com>
2021-09-10 14:32:34 +0200

    initial commit

---
+++ src/parsing/parsing_util.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import pandas
+
+def get_samples_for_topic(samples: pandas.DataFrame, topics: List[str]) -> pandas.DataFrame:
+    return samples[samples["topic"].isin(topics)]
+
+def get_fold_frame(samples: pandas.DataFrame, train_topics: List[str], dev_topics: List[str], test_topics: List[str]) -> pandas.DataFrame:
+    train_samples = get_samples_for_topic(samples, train_topics)
+    dev_samples = get_samples_for_topic(samples, dev_topics)
+    test_samples = get_samples_for_topic(samples, test_topics)
+
+    train_samples.loc[:,"set"] = "train"
+    dev_samples.loc[:,"set"] = "dev"
+    test_samples.loc[:,"set"] = "test"
+
+    return pandas.concat([train_samples, dev_samples, test_samples])
+
+
+def save_topic_folds(samples: pandas.DataFrame, fold_topics: List[List[str]], data_path: str, task_name: str):
+    for i, (train_topics, dev_topics, test_topics) in enumerate(fold_topics):
+        fold_frame = get_fold_frame(samples, train_topics, dev_topics, test_topics)
+        fold_frame[["id", "sentence1", "sentence2", "label", "set"]].to_csv(data_path + task_name + "_fold_" + str(i) + ".csv", index=False)
+
